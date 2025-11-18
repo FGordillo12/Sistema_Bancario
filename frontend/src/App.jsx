68e6fd55d409fdeb62020c7abf92974a2956f58a
sanCode <santoni251@hotmail.com>
2025-11-18 13:01:14 -0500

    Pagina principal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Login from './login.jsx'
 import Dashboard from './dashboard.jsx'
+import Registro from './Registro.jsx'
+import PaginaPrincipal from './PaginaPrincipal.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  const [user, setUser] = useState(null)
-
   return (
-    <div style={{ padding: '20px' }}>
-      {!user ? (
-        <Login onLogin={setUser} />
-      ) : (
-        <Dashboard user={user} onLogout={() => setUser(null)} />
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PaginaPrincipal />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </Router>
   )
 }
 
