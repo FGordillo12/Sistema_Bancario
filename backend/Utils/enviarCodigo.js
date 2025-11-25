d18e0bc88e5f8fe90163c2d9ca874add90098c04
sanCode <santoni251@hotmail.com>
2025-11-25 08:18:14 -0500

    modificacion correo

--- backend/Utils/enviarCodigo.js
+++ backend/Utils/enviarCodigo.js
@@ -3,28 +3,21 @@ import nodemailer from "nodemailer";
 export async function enviarCodigo(destinatario, codigo) {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
 
-    try {
-  await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: destinatario,
-        subject: "Código de verificación (2FA)",
-        text: `Tu código es: ${codigo}`,
-      });
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: destinatario,
+      subject: "Código de verificación (2FA)",
+      text: `Tu código es: ${codigo}`,
+    });
 
-      console.log("✅ Código enviado a", destinatario);
-    } catch (err) {
-      console.log("⚠️ No se pudo enviar el correo, pero continuamos:", err.message);
-    }
-
+    console.log("Código enviado a", destinatario);
   } catch (error) {
     console.error("Error enviando correo:", error);
   }
